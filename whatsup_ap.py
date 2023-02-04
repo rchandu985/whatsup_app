5ebbd67e76ca3baae352f7e132ac4ee71e2f02fd
Dheeraj1234564 <dheerajpandya12@gmail.com>
2023-02-04 22:27:39 +0530

    leaning about string

--- whatsup_ap.py
+++ whatsup_ap.py
@@ -6,3 +6,12 @@ print(s.index('a'))#grt index index number
 #len is key word
 print(len(s))
 
+#type converdstion
+string="ssfvijklmnldfjijifdjidjidfuhdfulhidfljsdfuivhdsfuvhsdfilvsidfvudfhildfmlkdfjlidfhlidfkbldfkmjodfhlidfjl/dfmkldfil"
+#based index
+i=string[0]
+#starting to ending
+s_e=string[0:4]#output ssfv
+#start only
+print(string[:])
+
