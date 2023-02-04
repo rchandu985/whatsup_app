832183a9fba8ec44b22bcb3b3a468faa0bcccf48
Dheeraj1234564 <dheerajpandya12@gmail.com>
2023-02-04 21:57:50 +0530

    added len

--- whatsup_ap.py
+++ whatsup_ap.py
@@ -1,4 +1,8 @@
 #type conversations  and strings information
 s='abcdefghjklga'
 print(s.index('a'))#grt index index number
+
+#taken only interable values variable
+#len is key word
 print(len(s))
+
