1a48d37896d527fa965f056e4ff23719f65ab0e9
Abhishek4268 <abhilolla002@gmail.com>
2023-02-04 22:31:00 +0530

    ho

--- whatsup_ap.py
+++
@@ -1,8 +0,0 @@
-#type conversations  and strings information
-s='abcdefghjklga'
-print(s.index('a'))#grt index index number
-
-#taken only interable values variable
-#len is key word
-print(len(s))
-
