c796f0cb1d1b480738318b487a5de895152bf7d1
Dheeraj1234564 <dheerajpandya12@gmail.com>
2023-02-04 22:39:46 +0530

    finalyzzing

---
+++ whatsup_ap.py
@@ -0,0 +1,18 @@
+#type conversations  and strings information
+s='abcdefghjklga'
+print(s.index('a'))#grt index index number
+
+#taken only interable values variable
+#len is key word
+print(len(s))
+
+#type converdstion
+string="ssfvijklmnldfjijifdjidjidfuhdfulhidfljsdfuivhdsfuvhsdfilvsidfvudfhildfmlkdfjlidfhlidfkbldfkmjodfhlidfjl/dfmkldfil"
+#based index
+i=string[0]
+#starting to ending
+s_e=string[0:4]#output ssfv
+#start only
+print(string[:])#it starts from staring variable to ending variable or number
+print(string[0:4])#it will starts from starting to 4th index
+print(string[5:])#it will starts from 5th index to end of the given sting
