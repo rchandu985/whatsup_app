a45d3d1e30774fd59bd91ca29411e362125ff7e7
Dheeraj1234564 <dheerajpandya12@gmail.com>
2023-02-04 22:50:34 +0530

    final code

--- whatsup_ap.py
+++ whatsup_ap.py
@@ -13,6 +13,5 @@ i=string[0]
 #starting to ending
 s_e=string[0:4]#output ssfv
 #start only
-print(string[:])#it starts from staring variable to ending variable or number
 print(string[0:4])#it will starts from starting to 4th index
-print(string[5:])#it will starts from 5th index to end of the given sting
+print(string[5:])#it will starts from 5th index to of the given sting
