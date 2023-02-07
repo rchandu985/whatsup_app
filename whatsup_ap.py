1657c27ddc6fdcda4e6be7a3a843f910625cf74a
Abhishek4268 <abhilolla002@gmail.com>
2023-02-07 20:39:31 +0530

    statements files

--- whatsup_ap.py
+++ whatsup_ap.py
@@ -1,5 +1,19 @@
 #conditional statements
 #single condition checking
+
+fruit=input("ENter the name of fruit")
 apple='apple'
 orange='orange'
 guva='guva'
+x="hi"
+y=input("enter the variable")
+if apple==fruit:
+    if y==x:
+        print("its an apple")
+    
+elif orange==fruit:
+    print(orange)
+elif guva==fruit:
+    print(guva)
+else:
+    print("not the fruit")
