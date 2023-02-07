16b1b8f0c986781b674d2397eee49a055113083d
Dheeraj1234564 <124327616+Dheeraj1234564@users.noreply.github.com>
2023-02-07 12:07:23 +0000

    string methods added

--- whatsup_ap.py
+++ whatsup_ap.py
@@ -15,3 +15,13 @@ s_e=string[0:4]#output ssfv
 #start only
 print(string[0:4])#it will starts from starting to 4th index
 print(string[5:])#it will starts from 5th index to of the given sting
+
+#String Methods:
+s="Dheeraj"
+print(s.upper())         #it coverts the all strings into the capital letters
+print(s.lower())         #it coverts all the strings into the small letters
+print("a" in s)          #it prints true if given string or num is present else false can be return
+print("1".isdigit())     #The isdigit() method returns True if all the characters are digits, otherwise False
+print("abc".isalpha())   #it can returns True if all the characters are alphabet letters (a-z).
+print("120".isdigit())   #The isdigit() method returns True if all the characters are digits, otherwise False
+
